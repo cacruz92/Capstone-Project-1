2c0383403612209d32f00a46d65b915136d9da96
Christian Cruz <christian.cruz92@hotmail.com>
2024-02-04 09:28:40 -0500

    added some user login/out/signup functionality and added base template

--- app.py
+++ app.py
@@ -24,11 +24,62 @@ app.config['SECRET_KEY'] = "BigBoyDeluxe"
 
 debug = DebugToolbarExtension(app)
 
+##############################################################################
+# User signup/login/logout
+##############################################################################
+
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global"""
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+    else:
+         g.user = None
+
+def do_login(user):
+     """Logs user in."""
+
+     session[CURR_USER_KEY] = user.id
+
+def do_logout(user):
+     """Logs user out."""
+
+     if CURR_USER_KEY in session:
+          del session[CURR_USER_KEY]
+
+
 @app.route('/')
 def root():
     """Homepage"""
-    return 'Welcome!'
+    return redirect('/register')
 
+
+@app.route('/register')
+def add_user():
+    """Handles the adding of a user"""
+
+    form = UserAddForm()
+
+    if form.validate_on_submit():
+        user = User.signup(
+            first_name = form.first_name.data
+            last_name = form.last_name.data
+            email = form.email.data
+            password = form.password.data
+            daily_calorie_goal = form.daily_calorie_goal.data
+            goal_weight = form.goal_weight.data
+            )
+        
+        db.session.commit()
+        
+        do_login(user)
+
+        return render_template('user.html')
+
+    else:
+        return render_template('users/signup.html')
+
+            
 
 if __name__ == '__main__':
     app.run(debug=True)
